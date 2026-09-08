be579a9c83c1ec9b049d39d87a8874fd2b9a1f68
Jonny Johannes <jonny@jonnyjohannes.com>
2026-09-07 22:37:08 -0400

    resolves:
    ```
    Error: Cask 'aesthetic-clock-screensaver' definition is invalid: undefined method 'home' for class Pathname
    ```

--- Casks/aesthetic-clock-screensaver.rb
+++ Casks/aesthetic-clock-screensaver.rb
@@ -10,13 +10,13 @@ cask "aesthetic-clock-screensaver" do
   screen_saver "AestheticClock-#{version}/AestheticClock.saver"
 
   preflight_steps do
-    system_command "rm", args: ["-rf", "#{Pathname.home}/Library/Screen Savers/AestheticClock.saver"]
+    system_command "rm", args: ["-rf", "#{Dir.home}/Library/Screen Savers/AestheticClock.saver"]
   end
 
   postflight_steps do
     if MacOS.version >= :big_sur
       system_command "xattr",
-                     args: ["-d", "com.apple.quarantine", "#{Pathname.home}/Library/Screen Savers/AestheticClock.saver"]
+                     args: ["-d", "com.apple.quarantine", "#{Dir.home}/Library/Screen Savers/AestheticClock.saver"]
     end
   end
 
